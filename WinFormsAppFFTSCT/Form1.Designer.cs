9e7a01293f2840dd0b15e55a01a4ed4adef9c7f3
Luiz Pedro Bittencourt <luizpedrob65@gmail.com>
2024-03-21 21:47:14 -0300

    More atualizations. Almost there!

--- WinFormsAppFFTSCT/Form1.Designer.cs
+++ WinFormsAppFFTSCT/Form1.Designer.cs
@@ -32,12 +32,24 @@ namespace WinFormsAppFFTSCT
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
-            System.Text.ASCIIEncoding asciiEncoding1 = new System.Text.ASCIIEncoding();
-            System.Text.DecoderReplacementFallback decoderReplacementFallback1 = new System.Text.DecoderReplacementFallback();
-            System.Text.EncoderReplacementFallback encoderReplacementFallback1 = new System.Text.EncoderReplacementFallback();
+            System.Text.ASCIIEncoding asciiEncoding3 = new System.Text.ASCIIEncoding();
+            System.Text.DecoderReplacementFallback decoderReplacementFallback3 = new System.Text.DecoderReplacementFallback();
+            System.Text.EncoderReplacementFallback encoderReplacementFallback3 = new System.Text.EncoderReplacementFallback();
             serialPort1 = new SerialPort(components);
             FftDisplayPoint = new ScottPlot.WinForms.FormsPlot();
             PointDisplayPlot = new ScottPlot.WinForms.FormsPlot();
+            serialCloseBtn = new Button();
+            chBoxSelectPort = new ComboBox();
+            chBoxBaudRate = new ComboBox();
+            label1 = new Label();
+            label2 = new Label();
+            serialOpenBtn = new Button();
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            button3 = new Button();
+            collecteDataBtn = new Button();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
             SuspendLayout();
             // 
             // serialPort1
@@ -46,9 +58,9 @@ namespace WinFormsAppFFTSCT
             serialPort1.DataBits = 8;
             serialPort1.DiscardNull = false;
             serialPort1.DtrEnable = false;
-            //asciiEncoding1.DecoderFallback = decoderReplacementFallback1;
-            //asciiEncoding1.EncoderFallback = encoderReplacementFallback1;
-            serialPort1.Encoding = asciiEncoding1;
+            //asciiEncoding3.DecoderFallback = decoderReplacementFallback3;
+            //asciiEncoding3.EncoderFallback = encoderReplacementFallback3;
+            serialPort1.Encoding = asciiEncoding3;
             serialPort1.Handshake = Handshake.None;
             serialPort1.NewLine = "\n";
             serialPort1.Parity = Parity.None;
@@ -65,29 +77,132 @@ namespace WinFormsAppFFTSCT
             // FftDisplayPoint
             // 
             FftDisplayPoint.DisplayScale = 1F;
-            FftDisplayPoint.Location = new Point(12, 11);
+            FftDisplayPoint.Location = new Point(14, 15);
+            FftDisplayPoint.Margin = new Padding(3, 4, 3, 4);
             FftDisplayPoint.Name = "FftDisplayPoint";
-            FftDisplayPoint.Size = new Size(417, 427);
+            FftDisplayPoint.Size = new Size(477, 569);
             FftDisplayPoint.TabIndex = 0;
             // 
             // PointDisplayPlot
             // 
             PointDisplayPlot.DisplayScale = 1F;
-            PointDisplayPlot.Location = new Point(395, 12);
+            PointDisplayPlot.Location = new Point(451, 16);
+            PointDisplayPlot.Margin = new Padding(3, 4, 3, 4);
             PointDisplayPlot.Name = "PointDisplayPlot";
-            PointDisplayPlot.Size = new Size(421, 426);
+            PointDisplayPlot.Size = new Size(466, 568);
             PointDisplayPlot.TabIndex = 1;
+            // 
+            // serialCloseBtn
+            // 
+            serialCloseBtn.Location = new Point(113, 26);
+            serialCloseBtn.Name = "serialCloseBtn";
+            serialCloseBtn.Size = new Size(84, 72);
+            serialCloseBtn.TabIndex = 2;
+            serialCloseBtn.Text = "Close";
+            serialCloseBtn.Click += serialCloseBtn_Click;
+            // 
+            // chBoxSelectPort
+            // 
+            chBoxSelectPort.Location = new Point(17, 104);
+            chBoxSelectPort.Name = "chBoxSelectPort";
+            chBoxSelectPort.Size = new Size(180, 28);
+            chBoxSelectPort.TabIndex = 3;
+            // 
+            // chBoxBaudRate
+            // 
+            chBoxBaudRate.Items.AddRange(new object[] { "1200", "2400", "4800", "9600", "19200", "38400", "57600", "115200" });
+            chBoxBaudRate.Location = new Point(17, 138);
+            chBoxBaudRate.Name = "chBoxBaudRate";
+            chBoxBaudRate.Size = new Size(180, 28);
+            chBoxBaudRate.TabIndex = 4;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(215, 109);
+            label1.Name = "label1";
+            label1.Size = new Size(113, 20);
+            label1.TabIndex = 7;
+            label1.Text = "Serial COM Port";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(215, 141);
+            label2.Name = "label2";
+            label2.Size = new Size(77, 20);
+            label2.TabIndex = 8;
+            label2.Text = "Baud Rate";
+            // 
+            // serialOpenBtn
+            // 
+            serialOpenBtn.Location = new Point(17, 26);
+            serialOpenBtn.Name = "serialOpenBtn";
+            serialOpenBtn.Size = new Size(84, 72);
+            serialOpenBtn.TabIndex = 13;
+            serialOpenBtn.Text = "Open";
+            serialOpenBtn.UseVisualStyleBackColor = true;
+            serialOpenBtn.Click += serialOpenBtn_Click;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(serialOpenBtn);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(chBoxBaudRate);
+            groupBox1.Controls.Add(chBoxSelectPort);
+            groupBox1.Controls.Add(serialCloseBtn);
+            groupBox1.Location = new Point(906, 34);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(397, 184);
+            groupBox1.TabIndex = 14;
+            groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(button3);
+            groupBox2.Controls.Add(collecteDataBtn);
+            groupBox2.Location = new Point(906, 224);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(397, 89);
+            groupBox2.TabIndex = 15;
+            groupBox2.TabStop = false;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(215, 26);
+            button3.Name = "button3";
+            button3.Size = new Size(162, 47);
+            button3.TabIndex = 1;
+            button3.Text = "Plot FFT";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // collecteDataBtn
+            // 
+            collecteDataBtn.Location = new Point(17, 26);
+            collecteDataBtn.Name = "collecteDataBtn";
+            collecteDataBtn.Size = new Size(162, 47);
+            collecteDataBtn.TabIndex = 0;
+            collecteDataBtn.Text = "Collect Data";
+            collecteDataBtn.UseVisualStyleBackColor = true;
+            collecteDataBtn.Click += collecteDataBtn_Click;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(835, 471);
+            ClientSize = new Size(1396, 636);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
             Controls.Add(PointDisplayPlot);
             Controls.Add(FftDisplayPoint);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
-            Text = "Form1";
+            Text = "FFT Serial Plotter";
             Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -95,5 +210,24 @@ namespace WinFormsAppFFTSCT
 
         private ScottPlot.WinForms.FormsPlot FftDisplayPoint;
         private ScottPlot.WinForms.FormsPlot PointDisplayPlot;
+        private Button button1;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private ComboBox comboBox3;
+        private ComboBox comboBox4;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private ComboBox comboBox5;
+        private Button serialOpenBtn;
+        private GroupBox groupBox1;
+        private ComboBox chBoxBaudRate;
+        private Button serialCloseBtn;
+        private ComboBox chBoxSelectPort;
+        private GroupBox groupBox2;
+        private Button button3;
+        private Button collecteDataBtn;
     }
 }
--- WinFormsAppFFTSCT/Form1.cs
+++ WinFormsAppFFTSCT/Form1.cs
@@ -14,53 +14,69 @@ namespace WinFormsAppFFTSCT
         }
         private void Form1_Load(object sender, EventArgs e)
         {
-            double[] dataX = { 1, 2, 3, 4, 5 };
-            double[] dataY = { 1, 4, 8, 16, 25 };
+            string[] availablePorts = SerialPort.GetPortNames();
+            chBoxSelectPort.Items.AddRange(availablePorts);
 
-            double[] time = {0, 8e-9, 1.6e-8, 2.4e-8, 3.2e-8, 4e-8, 4.8e-8, 5.6e-8,
-                        6.4e-8, 7.2e-8, 8e-8, 8.8e-8, 9.6e-8, 1.04e-7, 1.12e-7, 1.2e-7,
-                        1.28e-7, 1.36e-7, 1.44e-7, 1.52e-7, 1.6e-7, 1.68e-7, 1.76e-7, 1.84e-7,
-                        1.92e-7, 2e-7, 2.08e-7, 2.16e-7, 2.24e-7, 2.32e-7, 2.4e-7, 2.48e-7,
-                        2.56e-7, 2.64e-7, 2.72e-7, 2.8e-7, 2.88e-7, 2.96e-7, 3.04e-7, 3.12e-7,
-                        3.2e-7, 3.28e-7, 3.36e-7, 3.44e-7, 3.52e-7, 3.6e-7, 3.68e-7, 3.76e-7,
-                        3.84e-7, 3.92e-7, 4e-7, 4.08e-7, 4.16e-7, 4.24e-7, 4.32e-7, 4.4e-7,
-                        4.48e-7, 4.56e-7, 4.64e-7, 4.72e-7, 4.8e-7, 4.88e-7, 4.96e-7, 5.04e-7,
-                        5.12e-7, 5.2e-7, 5.28e-7, 5.36e-7, 5.44e-7, 5.52e-7, 5.6e-7, 5.68e-7,
-                        5.76e-7, 5.84e-7, 5.92e-7, 6e-7, 6.08e-7, 6.16e-7, 6.24e-7, 6.32e-7,
-                        6.4e-7, 6.48e-7, 6.56e-7, 6.64e-7, 6.72e-7, 6.8e-7, 6.88e-7, 6.96e-7,
-                        7.04e-7, 7.12e-7, 7.2e-7, 7.28e-7, 7.36e-7, 7.44e-7, 7.52e-7, 7.6e-7,
-                        7.68e-7, 7.76e-7, 7.84e-7, 7.92e-7, 8e-7
-                        };
-            
-            double[] dados_ch1 = {
-              75,   95,   32,   31,   99,   -32,   -31,   93,
-              -95,   32,   -96,   31,   33,   96,   34,   32,
-              37,   -98,   99,   96,   -30,   31,   -32,   30,
-              36,   -184,   1656,   1646,   1177,   433,   -1451,   -1844,
-              -1839,   -377,   -102,   102,   234,   105,   232,   230,
-              41,   104,   169,   228,   169,   174,   369,   296,
-              -367,   -499,   -170,   -108,   -97,   98,   172,   302,
-              368,   362,   107,   -259,   -234,   -111,   -103,   30,
-              -230,   -511,   -404,   -252,   -29,   96,   101,   98,
-              229,   97,   33,   30,   100,   93,   101,   32,
-              34,   -31,   -138,   32,   -94,   -33,   -95,   98,
-              162,   -33,   -97,   -99,   97,   31,   35,   34,
-              37,   -28,   165,   98,   -98,    15,    0,    0,    0,    0,    0,
-              0,    0,    0,    0,    0,    0,     0,    0,    0,    0,    0,    0,
-              0,    0,    0,    0,    0,    0,     0,    0,    0};
+            serialCloseBtn.Enabled = false;
+            serialOpenBtn.Enabled = true;
+        }
 
-            int sampleRate = 125000000;
+        private void serialOpenBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.PortName = chBoxSelectPort.Text;
+                serialPort1.BaudRate = Convert.ToInt32(chBoxBaudRate.Text);
+                serialPort1.Open();
 
-            System.Numerics.Complex[] aqsData_FFT = FftSharp.FFT.Forward(dados_ch1);
-            double[] psd = FftSharp.FFT.Power(aqsData_FFT);
-            double[] freq = FftSharp.FFT.FrequencyScale(aqsData_FFT.Length, sampleRate);
+                serialCloseBtn.Enabled = true;
+                serialOpenBtn.Enabled = false;
+                MessageBox.Show("Serial Port " + serialPort1.PortName + " succesfully opened.", "Attention", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
-            FftDisplayPoint.Plot.Add.Scatter(freq, psd);
-            FftDisplayPoint.Refresh();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
 
-            PointDisplayPlot.Plot.Add.Scatter(time, dados_ch1);
-            PointDisplayPlot.Refresh();
+        private void serialCloseBtn_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
 
+                MessageBox.Show("Serial Port " + serialPort1.PortName + " succesfully closed.", "Attention", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                serialCloseBtn.Enabled = false;
+                serialOpenBtn.Enabled = true;
+            }
+        }
+
+        private void collecteDataBtn_Click(object sender, EventArgs e)
+        {
+            double[] test = returnDataArray();
+            MessageBox.Show("Data succesfully collected. Here's the 16° number of the array: " + test[15], "Attention", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+        }
+
+        private double[] returnDataArray()
+        {
+            string notFormattedData;
+            int count = 0;
+            double currentData;
+            double[] collectedData = new double[128];
+
+            while ((serialPort1.IsOpen) && (count < 128))
+            {
+                notFormattedData = serialPort1.ReadLine();
+                if(double.TryParse(notFormattedData, out currentData))
+                {
+                    collectedData[count] = currentData;
+                    count++;
+                }
+            }
+            return(collectedData);
         }
     }
 }
